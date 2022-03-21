de560855934fd8da17b102ebf434e66345c59d6d
lockedinbasement <dariusz.strych.lib@gmail.com>
2022-03-21 22:05:26 +0100

    new proj start

--- test1-app/src/app/components/header/header.component.ts
+++ test1-app/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title: string = 'test1-app';
+  showAddTask!:boolean ;
+  subsciption!:Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) {
+    this.subsciption = this.uiService.onToggle().subscribe( (value) => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle bbbbbbb ');
+    this.uiService.toggleAddTask();
   }
 }
